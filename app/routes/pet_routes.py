b6e8fcbef69b9ed3652b5e195c7bf7937ea19659
Tatiana Snook <tatsnook@gmail.com>
2024-12-09 13:56:13 -0600

    initial commit

--- app/routes/pet_routes.py
+++ app/routes/pet_routes.py
@@ -1,12 +1,41 @@
 from flask import Blueprint, request, abort, make_response
 from ..db import db
 from ..models.pet import Pet
+import google.generativeai as genai
+import os
+
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
 bp = Blueprint("pets", __name__, url_prefix="/pets")
 
+
+def generate_name():
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    input_message = f"I have a pet model and can you generate a name."
+    response = model.generate_content(input_message)
+    return response
+
 @bp.post("")
 def create_pet():
-    pass
+    request_body = request.get_json()
+    name['name'] = generate_name(request_body)
+    print(name)
+    
+    try:
+        new_pet = Pet.from_dict(request_body)
+
+        db.session.add(new_pet)
+        db.session.commit()
+
+        return new_pet.to_dict(), 201
+
+    except KeyError as e:
+        abort(make_response({"message": f"missing required value: {e}"}, 400))
+
+    
+    # response_split = response.text.split("\n")
+    # print(response_split)
+    # return response_split[:-1]
 
 @bp.get("")
 def get_pets():
